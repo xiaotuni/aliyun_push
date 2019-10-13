47eb8acedd92620a4340bc22444ed423d7953b23
xiaotuni <xiaotuni@gmail.com>
2019-10-13 17:00:55 +0800

    注释掉连接redis代码。

--- server/service/RedisClientService.js
+++ server/service/RedisClientService.js
@@ -8,20 +8,20 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 export function createRedisClient(opt) {
-  const rc = redis.createClient(opt.port, opt.host, opt);
-  rc.on('error', (err) => {
-    Utility.printLog('Redis Error:', opt.host, err);
-  });
+  // const rc = redis.createClient(opt.port, opt.host, opt);
+  // rc.on('error', (err) => {
+  //   Utility.printLog('Redis Error:', opt.host, err);
+  // });
 
-  rc.on('end', (err) => {
-    Utility.printLog('Redis end:', opt.host, err);
-  });
+  // rc.on('end', (err) => {
+  //   Utility.printLog('Redis end:', opt.host, err);
+  // });
 
-  rc.on('ready', (err) => {
-    Utility.printLog('Redis ready:', opt.host, err);
-  });
+  // rc.on('ready', (err) => {
+  //   Utility.printLog('Redis ready:', opt.host, err);
+  // });
 
-  return rc;
+  // return rc;
 }
 
 export default createRedisClient(config.redis);
